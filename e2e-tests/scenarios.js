7206f6a6cf26bb4f0092619b67a4cacfe189358c
Bhargavi-Kodidela <bhargavi@vivasa.in>
2017-01-27 11:39:37 +0530

    Update scenarios.js

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -14,10 +14,10 @@ describe('Mesaforte-ui Demo App', function(){
 
     //browser.wait(element(by.css(".topnavbar")).isPresent());
     //Filter Results
-    expect(element(by.model('beanCtrl.gridOptions.quickFilterText12')).isPresent()).toBe(true);
+    expect(element(by.model('beanCtrl.gridOptions.quickFilterText')).isPresent()).toBe(true);
     element(by.model('beanCtrl.gridOptions.quickFilterText')).sendKeys('dent');
     //Search
-    expect(element(by.model('beanCtrl.pages.category1')).isPresent()).toBe(true);
+    expect(element(by.model('beanCtrl.pages.category')).isPresent()).toBe(true);
     element(by.model('beanCtrl.pages.category')).sendKeys('built-in');
     element(by.buttonText('Search')).click();
     
